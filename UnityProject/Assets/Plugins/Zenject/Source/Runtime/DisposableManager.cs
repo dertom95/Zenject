ad3b7517ce31a3ba162a8bae0bcc99cd5a4944a2
Thomas Trocha <thomas.trocha@gmail.com>
2018-06-29 10:52:07 +0200

    make it possible to skip final dispose

--- UnityProject/Assets/Plugins/Zenject/Source/Runtime/DisposableManager.cs
+++ UnityProject/Assets/Plugins/Zenject/Source/Runtime/DisposableManager.cs
@@ -12,6 +12,11 @@ namespace Zenject
         bool _disposed;
         bool _lateDisposed;
 
+        /// <summary>
+        /// Skip dispose if wanted
+        /// </summary>
+        bool _skipDispose;
+
         [Inject]
         public DisposableManager(
             [Inject(Optional = true, Source = InjectSources.Local)]
@@ -21,10 +26,8 @@ namespace Zenject
             [Inject(Optional = true, Source = InjectSources.Local)]
             List<ILateDisposable> lateDisposables,
             [Inject(Id = "Late", Optional = true, Source = InjectSources.Local)]
-            List<ModestTree.Util.ValuePair<Type, int>> latePriorities)
-        {
-            foreach (var disposable in disposables)
-            {
+            List<ModestTree.Util.ValuePair<Type, int>> latePriorities) {
+            foreach (var disposable in disposables) {
                 // Note that we use zero for unspecified priority
                 // This is nice because you can use negative or positive for before/after unspecified
                 var match = priorities.Where(x => disposable.GetType().DerivesFromOrEqual(x.First)).Select(x => (int?)x.Second).SingleOrDefault();
@@ -33,8 +36,7 @@ namespace Zenject
                 _disposables.Add(new DisposableInfo(disposable, priority));
             }
 
-            foreach (var lateDisposable in lateDisposables)
-            {
+            foreach (var lateDisposable in lateDisposables) {
                 var match = latePriorities.Where(x => lateDisposable.GetType().DerivesFromOrEqual(x.First)).Select(x => (int?)x.Second).SingleOrDefault();
                 int priority = match.HasValue ? match.Value : 0;
 
@@ -42,36 +44,34 @@ namespace Zenject
             }
         }
 
-        public void Add(IDisposable disposable)
-        {
+        public void Add(IDisposable disposable) {
             Add(disposable, 0);
         }
 
-        public void Add(IDisposable disposable, int priority)
-        {
+        public void Add(IDisposable disposable, int priority) {
             _disposables.Add(
                 new DisposableInfo(disposable, priority));
         }
 
-        public void AddLate(ILateDisposable disposable)
-        {
+        public void AddLate(ILateDisposable disposable) {
             AddLate(disposable, 0);
         }
 
-        public void AddLate(ILateDisposable disposable, int priority)
-        {
+        public void AddLate(ILateDisposable disposable, int priority) {
             _lateDisposables.Add(
                 new LateDisposableInfo(disposable, priority));
         }
 
-        public void Remove(IDisposable disposable)
-        {
+        public void Remove(IDisposable disposable) {
             _disposables.RemoveWithConfirm(
                 _disposables.Where(x => x.Disposable == disposable).Single());
         }
 
-        public void LateDispose()
-        {
+        public void LateDispose() {
+            if (_skipDispose) {
+                return;
+            }
+
             Assert.That(!_lateDisposed, "Tried to late dispose DisposableManager twice!");
             _lateDisposed = true;
 
@@ -85,22 +85,22 @@ namespace Zenject
             }
 #endif
 
-            foreach (var disposable in disposablesOrdered)
-            {
-                try
-                {
+            foreach (var disposable in disposablesOrdered) {
+                try {
                     disposable.LateDisposable.LateDispose();
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) {
                     throw Assert.CreateException(
                         e, "Error occurred while late disposing ILateDisposable with type '{0}'", disposable.LateDisposable.GetType());
                 }
             }
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
+            if (_skipDispose) {
+                return;
+            }
+
             Assert.That(!_disposed, "Tried to dispose DisposableManager twice!");
             _disposed = true;
 
@@ -114,18 +114,23 @@ namespace Zenject
             }
 #endif
 
-            foreach (var disposable in disposablesOrdered)
-            {
-                try
-                {
+            foreach (var disposable in disposablesOrdered) {
+                try {
                     disposable.Disposable.Dispose();
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) {
                     throw Assert.CreateException(
                         e, "Error occurred while disposing IDisposable with type '{0}'", disposable.Disposable.GetType());
                 }
             }
+        }
+
+        /// <summary>
+        /// Skip the dispose process
+        /// </summary>
+        /// <param name="skipDispose"></param>
+        public void SkipDispose(bool skipDispose) {
+            _skipDispose = skipDispose;
         }
 
         struct DisposableInfo
@@ -133,8 +138,7 @@ namespace Zenject
             public IDisposable Disposable;
             public int Priority;
 
-            public DisposableInfo(IDisposable disposable, int priority)
-            {
+            public DisposableInfo(IDisposable disposable, int priority) {
                 Disposable = disposable;
                 Priority = priority;
             }
@@ -145,8 +149,7 @@ namespace Zenject
             public ILateDisposable LateDisposable;
             public int Priority;
 
-            public LateDisposableInfo(ILateDisposable lateDisposable, int priority)
-            {
+            public LateDisposableInfo(ILateDisposable lateDisposable, int priority) {
                 LateDisposable = lateDisposable;
                 Priority = priority;
             }
